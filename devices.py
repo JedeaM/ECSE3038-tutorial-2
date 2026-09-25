83e768c9ceeec71a879db002b6629d923fcc6b5e
JedeaM <kxngjd0@gmail.com>
2026-09-24 22:35:44 -0500

    task 5: group by room

--- devices.py
+++ devices.py
@@ -48,3 +48,18 @@ def to_status(device):
     }
 
 print(to_status(readings[3]))
+
+def by_room(devices):
+    rooms = {}
+
+    for device in devices:
+        room = device["room"]
+
+        if room not in rooms:
+            rooms[room] = []
+
+        rooms[room].append(device["name"])
+
+    return rooms
+
+print(by_room(readings))
